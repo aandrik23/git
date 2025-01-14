899e0cce450d2d8fb63d7f0b3069aa99bb413f1c
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-01-14 18:30:19 +0200

    Add greeter.sh with a greeter function

--- lib/greeter.sh
+++ lib/greeter.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
+ #!/bin/bash
 
-source lib/greeter.sh
-
-name="$1"
-if [ -z "$name" ]; then
-    name="World"
-fi
-
-Greeter "$name"
+Greeter() {
+    who="$1"
+    echo "Hello, $who"
+}
